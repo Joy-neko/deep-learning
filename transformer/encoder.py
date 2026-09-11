534f011e166a0d9216bf82f5b96bc97d0ee490b9
Joy-neko <3359034595@qq.com>
2026-09-11 10:14:11 +0800

    Update encoder.py

--- transformer/encoder.py
+++ transformer/encoder.py
@@ -1,1 +1,112 @@
+import torch
+from torch import nn
+
+from attention import MultiHeadAttention
+from embedding import TransformerEmbedding
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-12):
+        super(LayerNorm, self).__init__()
+
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, unbiased=False, keepdim=True)
+
+        out = (x - mean) / torch.sqrt(var + self.eps)
+        out = self.gamma * out + self.beta
+        return out
+
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, hidden, drop_prob=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
+        super(EncoderLayer, self).__init__()
+
+        self.attention = MultiHeadAttention(d_model, n_head)
+        self.norm1 = LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.ffn = PositionwiseFeedForward(d_model, ffn_hidden, drop_prob)
+        self.norm2 = LayerNorm(d_model)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+    def forward(self, x, src_mask):
+        residual = x
+        x = self.attention(
+            q=x,
+            k=x,
+            v=x,
+            mask=src_mask
+        )
+        x = self.dropout1(x)
+        x = self.norm1(x + residual)
+
+        residual = x
+        x = self.ffn(x)
+        x = self.dropout2(x)
+        x = self.norm2(x + residual)
+
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(
+        self,
+        enc_voc_size,
+        max_len,
+        d_model,
+        ffn_hidden,
+        n_head,
+        n_layers,
+        drop_prob,
+        pad_idx,
+        device
+    ):
+        super(Encoder, self).__init__()
+
+        self.emb = TransformerEmbedding(
+            enc_voc_size,
+            d_model,
+            max_len,
+            drop_prob,
+            pad_idx,
+            device
+        )
+
+        self.layers = nn.ModuleList([
+            EncoderLayer(
+                d_model,
+                ffn_hidden,
+                n_head,
+                drop_prob
+            )
+            for _ in range(n_layers)
+        ])
+
+    def forward(self, x, src_mask):
+        x = self.emb(x)
+        for layer in self.layers:
+            x = layer(x, src_mask)
+        return x
 
